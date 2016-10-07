2365c91466e48c56befbc12e1be6bbb02f10dd1a
Matt Boynes <mboynes@alleyinteractive.com>
2016-10-07 15:23:55 -0400

    Attempt to catch js error

--- tests/phantomjs/hello.js
+++ tests/phantomjs/hello.js
@@ -1,5 +1,18 @@
 var page = require('webpage').create();
 
+page.onError = function(msg, trace) {
+	var msgStack = ['PAGE JS ERROR: ' + msg];
+
+	if (trace && trace.length) {
+		msgStack.push('TRACE:');
+		trace.forEach(function(t) {
+			msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function +'")' : ''));
+		});
+	}
+
+	console.error(msgStack.join('\n'));
+};
+
 phantom.onError = function(msg, trace) {
 	var msgStack = ['PHANTOM ERROR: ' + msg];
 	if (trace && trace.length) {
