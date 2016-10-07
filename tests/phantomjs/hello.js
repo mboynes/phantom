bdbcb193a812a2a100a4504172c29e2801d2c3b0
Matt Boynes <mboynes@alleyinteractive.com>
2016-10-07 15:35:39 -0400

    Consolidate error management

--- tests/phantomjs/hello.js
+++ tests/phantomjs/hello.js
@@ -1,4 +1,5 @@
 var page = require('webpage').create();
+var hasErrors = false;
 
 page.onError = function(msg, trace) {
 	var msgStack = ['PAGE JS ERROR: ' + msg];
@@ -11,7 +12,7 @@ page.onError = function(msg, trace) {
 	}
 
 	console.error(msgStack.join('\n'));
-	phantom.exit(1);
+	hasErrors = true;
 };
 
 phantom.onError = function(msg, trace) {
@@ -23,17 +24,18 @@ phantom.onError = function(msg, trace) {
 		});
 	}
 	console.error(msgStack.join('\n'));
-	phantom.exit(1);
+	hasErrors = true;
 };
 
 page.onResourceError = function(resourceError) {
-  console.log('Unable to load resource (#' + resourceError.id + 'URL:' + resourceError.url + ')');
-  console.log('Error code: ' + resourceError.errorCode + '. Description: ' + resourceError.errorString);
+	console.log('Unable to load resource (#' + resourceError.id + 'URL:' + resourceError.url + ')');
+	console.log('Error code: ' + resourceError.errorCode + '. Description: ' + resourceError.errorString);
+	hasErrors = true;
 };
 
 page.open('http://127.0.0.1:8080', function(status) {
 	console.log("Status: " + status);
-	if (status === "success") {
+	if (status === "success" && !hasErrors) {
 		phantom.exit();
 	} else {
 		phantom.exit(1);
